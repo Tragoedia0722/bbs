6fbfea05411be8ad75a802ed16f5045b1a031a0a
Tragoedia <tragedywr@gmail.com>
2022-03-29 21:18:50 +0800

    Init

---
+++ src/main/java/cn/tragoedia/bbs/MainApplication.java
@@ -0,0 +1,13 @@
+package cn.tragoedia.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
